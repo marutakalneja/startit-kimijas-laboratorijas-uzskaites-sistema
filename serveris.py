857c8db551d53a0932b4b710a74bc6c29ebb8588
Maruta Kalnēja <maruta.kalneja@gmail.com>
2020-12-29 09:20:42 +0000

    Izveidots ceļš (route) sveiki

--- serveris.py
+++ serveris.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+    return "Labrīt!!!"
 
+@app.route("/sveiki")
+def sveiki():
+  return "Nav vairs nekāds rīts!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
