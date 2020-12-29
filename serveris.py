501fe6fffe850b870400da9e9e0393a00d1bdb19
Maruta Kalnēja <maruta.kalneja@gmail.com>
2020-12-29 10:26:51 +0000

    Pievienoti ceļi vielām

--- serveris.py
+++ serveris.py
@@ -1,13 +1,35 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import dati
 
 app = Flask(__name__)
 
+app.config["JSON_AS_ASCII"]=False #džeisonu nevajag apstrādāt ar šo kodu tabulu
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+  return "Labrīt!"
 
+#izveidot route /api/v1/vielas, kas atgriež visus piemēra datus, kas atrodami datnē dati.py mainīgajā vielas. Ievērot, ka jāatgriež ir string datu tips
 
-if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True)
+@app.route("/api/v1/vielas")
+def vielas():
+  return jsonify(dati.vielas) # ja jāatgriež džeison dati, tad izmanto jsonify
+
+#izveidot route /api/v1/viela/<vielasID>, kas atgriež datus par vielu ar ID <vielasID>, kas atrodami datnē dati.py mainīgajā vielas. Ievērot, ka jāatgriež ir string datu tips
+
+@app.route("/api/v1/viela/<vielasID>")
+def viela(vielasID):
+
+  viela=f"Viela ar doto id {vielasID} nav atrasta"
+
+  for v in dati.vielas:
+    if str(v["id"])==vielasID:
+      viela=v
+  
+  return jsonify(viela)
+
+#izveidot route /api/v1/inventars, kas atgriež visus piemēra datus, kas atrodami datnē dati.py mainīgajā inventars
+
+@app.route("/api/v1/inventars")
+def inventars():
+  return jsonify(dati.inventars)
